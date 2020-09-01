541006433f3a37ee1a99381f0e51dd7dddfb9453
Kevin Radtke <kevinradtkeg@gmail.com>
2020-09-01 21:15:00 +0000

    refactor: move create_instance func to create_or_restore

--- instance/create_or_restore_instance.py
+++ instance/create_or_restore_instance.py
@@ -149,3 +149,51 @@ def create_or_restore_instance(driver):
     print('launched cuttlefish on', instance_name)
 
     return {"name": instance_name}
+
+
+def create_instance(driver):
+    """Creates a new Cuttlefish instance and launches it.
+       Does not store userdata.img in external GCP disk."""
+
+    # SETUP
+
+    args.target = args.target.replace('_','-')
+    instance_name = f'halyard-{args.user_id}'
+    image_family = f'halyard-{args.branch}-{args.target}'
+
+    try:
+        driver.ex_get_image_from_family(image_family)
+    except:
+        utils.fatal_error(f'Image family {image_family} does not exist.\n \
+            New base images can be created using the `create_base_image` endpoint.')
+
+    # Stops execution if instance already exists
+    instance = utils.find_instance(driver, instance_name, args.zone)
+    if instance:
+        utils.fatal_error(f'Instance {instance_name} already exists.')
+
+    build_node = driver.create_node(
+        instance_name,
+        'n1-standard-4',
+        None,
+        location=args.zone,
+        ex_image_family=image_family,
+        ex_service_accounts=[{'scopes': ['storage-ro']}],
+        ex_disk_size=30,
+        ex_tags=args.tags)
+
+    utils.wait_for_instance(instance_name, args.zone)
+
+    print('successfully created new instance', instance_name)
+
+    os.system(f'gcloud compute ssh --zone={args.zone} {instance_name} -- \
+        HOME=/usr/local/share/cuttlefish /usr/local/share/cuttlefish/bin/launch_cvd \
+        --start_webrtc --daemon \
+        --webrtc_sig_server_addr={args.sig_server_addr} \
+        --webrtc_sig_server_port={args.sig_server_port} \
+        --start_webrtc_sig_server=false \
+        --webrtc_device_id={instance_name}')
+
+    print('launched cuttlefish on', instance_name)
+
+    return {"name": instance_name}
